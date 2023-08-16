7f2f02ebe1e65c38b13906132f2123175d70d67f
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-08-16 07:44:18 +0200

    Ajout du composant édit

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,11 @@
 import NavigationHeader from '../components/header/NavigationHeader';
 import Account from '../components/account/Account';
+import EditUserInfo from '../components/edit-user-info/EditUserInfo';
+import { useState } from 'react';
 
 function User() {
+  const [hideForm, setHideForm] = useState(false);
+
   return (
     <>
       <NavigationHeader name="Tony" />
@@ -11,7 +15,18 @@ function User() {
             Welcome back
             <br /> {'Tony Jarvis'}!
           </h1>
-          <button className="edit-button">Edit Name</button>
+          <button
+            className="edit-button"
+            onClick={() => setHideForm(!hideForm)}
+          >
+            Edit Name
+          </button>
+          <EditUserInfo
+            form_title="Edit info"
+            save_button="save"
+            cancel_button="cancel"
+            display={hideForm}
+          />
         </div>
         <h2 className="sr-only">Accounts</h2>
         <Account
