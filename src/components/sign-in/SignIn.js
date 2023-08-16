d9f1555c4dae28d984a9e425758ebf723fe4ab17
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-08-16 07:44:46 +0200

    Création du composant SignIn

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+// import { useFetcher } from 'react-router-dom';
+
 function SignIn({
   icon,
   alt,
@@ -7,28 +10,69 @@ function SignIn({
   tick_text,
   text_button,
 }) {
+  // // eslint-disable-next-line no-unused-vars
+  // const fetch = useFetcher;
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
+  function handleInputemailChanges(e) {
+    setEmail(e.target.value);
+  }
+  function handleInputPasswordChanges(e) {
+    setPassword(e.target.value);
+  }
+
+  async function handlClick(e) {
+    e.preventDefault();
+
+    let user = {
+      email: email,
+      password: password,
+    };
+    let response = await fetch('http://localhost:3001/api/v1/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(user),
+    });
+    const responseBody = await response.json();
+    let status = responseBody.status;
+    setToken(responseBody.body.token);
+  }
+
   return (
     <>
-      <i className="fa fa-user-circle sign-in-icon">
+      <i className="fa fa-email-circle sign-in-icon">
         <img src={icon} alt={alt} />
       </i>
       <h1>{title}</h1>
       <form>
         <div className="input-wrapper">
-          <label htmlFor="username">{form_account}</label>
-          <input type="text" id="username" />
+          <label htmlFor="emailname">{form_account}</label>
+          <input
+            type="text"
+            id="emailname"
+            onChange={handleInputemailChanges}
+          />
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">{form_password}</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            onChange={handleInputPasswordChanges}
+          />
         </div>
         <div className="input-remember">
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">{tick_text}</label>
         </div>
-        <a href="./user" className="sign-in-button">
+        <button type="submit" className="sign-in-button" onClick={handlClick}>
           {text_button}
-        </a>
+        </button>
       </form>
     </>
   );
