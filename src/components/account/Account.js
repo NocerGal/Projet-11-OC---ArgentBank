5649b9d4057a8984c8bcecfe9cde79ed6aaf0dbf
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-08-16 07:43:57 +0200

    Correction de l'attribut class qui était écrit comme pour du HTML et non pour du React

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -4,10 +4,10 @@ function Account({ title, amount, description, button }) {
       <div className="account-content-wrapper">
         <h3 className="account-title">{title}</h3>
         <p className="account-amount">{amount}</p>
-        <p class="account-amount-description">{description}</p>
+        <p className="account-amount-description">{description}</p>
       </div>
-      <div class="account-content-wrapper cta">
-        <button class="transaction-button">{button}</button>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">{button}</button>
       </div>
     </section>
   );
