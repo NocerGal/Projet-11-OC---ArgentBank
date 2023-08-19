9aa968a31dbd099aa2592d0d39b70ac1185e20c9
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-08-19 16:58:09 +0200

    Création des routes API

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,33 +1,59 @@
-import signInError from '../../errors/signInError';
+// API base URL
+const baseUrl = 'http://localhost:3001/api/v1/';
 
-export default async function callApi(e, email, password) {
-  e.preventDefault();
+// POST
+async function postAccessToToken(email, password) {
   let user = {
     email: email,
     password: password,
   };
+  let response = await fetch(`${baseUrl}user/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  });
+  return response;
+}
 
-  try {
-    let response = await fetch('http://localhost:3001/api/v1/user/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(user),
-    });
-    const responseBody = await response.json();
-    const status = responseBody.status;
+async function postSignUp(email, password, firstName, lastName, userName) {
+  let newUser = {
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+    userName: userName,
+  };
+  let response = await fetch(`${baseUrl}user/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(newUser),
+  });
+  return response;
+}
 
-    if (status === 200) {
-      const token = responseBody.body.token;
-      // window.location.replace('./user');
-      console.log(token);
-      return token;
-    } else {
-      signInError();
-      return null;
-    }
-  } catch (error) {
-    console.log(error);
-  }
+// GET
+export async function getToken(email, password) {
+  const response = await postAccessToToken(email, password);
+  const responseBody = await response.json();
+  const token = responseBody.body.token;
+  return token;
+}
+
+// POST
+async function putChangeUserName() {
+  let newUserName = {
+    newUserName: newUserName,
+  };
+
+  fetch(`${baseUrl}/user/profile`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newUserName), //
+  });
 }
